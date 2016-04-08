8231d965d17ecbe002c6cbc4fd7add88b6476d0c
Alex Atkins <abatkins@email.wm.edu>
2016-04-08 14:48:34 -0400

    bugfixes and added params

--- hadaly_grid.py
+++ hadaly_grid.py
@@ -1,6 +1,6 @@
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.neural_network import BernoulliRBM
-from sklearn.pipeline import  Pipeline
+from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from MPINestedGridSearchCV import NestedGridSearchCV
 from sklearn.grid_search import GridSearchCV
@@ -8,6 +8,8 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import make_scorer
 from get_variables import VariablesXandY
 import logging
+from sklearn.externals import joblib
+
 
 def main():
     LOG_FILENAME = 'logs/gridsearch.log'
@@ -21,17 +23,17 @@ def main():
     x_train = variables_object.get_x_matrix(n_gram)
 
     #rbm = BernoulliRBM(random_state=0, verbose=True)
-    rbm = BernoulliRBM(batch_size=10, learning_rate=0.1, n_components=3, n_iter=10, random_state=0, verbose=True)
+    rbm = BernoulliRBM(random_state=0, verbose=True)
     svc = LinearSVC(C=1000, class_weight="auto")
     pipe = Pipeline(steps=[('rbm', rbm), ('svc', svc)])
 
     model_to_set = OneVsRestClassifier(pipe, n_jobs=1)
 
     parameters = {
-        "estimator__rbm__batch_size": [5], #[5,10]
-        "estimator__rbm__learning_rate": [.06],#[.001, .01, .06, .1],
-        "estimator__rbm__n_iter": [5],#[1,2,4,8,10],
-        "estimator__rbm__n_components": [2]#[1,5,10,20,100,256]
+        "estimator__rbm__batch_size": [5,10], #[5,10]
+        "estimator__rbm__learning_rate": [.06,.1],#[.001, .01, .06, .1],
+        "estimator__rbm__n_iter": [2,5],#[1,2,4,8,10],
+        "estimator__rbm__n_components": [3,5]#[1,5,10,20,100,256]
     }
 
     f1_scorer = make_scorer(f1_score, average='samples')
@@ -43,10 +45,13 @@ def main():
 
     model_tunning.fit(x_train.toarray(), y_train.toarray())
     output_path = '../scr00/output/output.pkl'
-    joblib.dump(classifier, output_path)
+    joblib.dump(model_tunning, output_path)
 
     logging.info("best score: " + str(model_tunning.best_score_))
-    loggin.info("best params: " + str(model_tunning.best_params_))
+    logging.info("best params: " + str(model_tunning.best_params_))
+
+    logging.info("best estimator: " + str(model_tunning.best_estimator_))
+    logging.info("grid scores: " + str(model_tunning.grid_scores_))
 
     print(model_tunning.best_score_)
     print(model_tunning.best_params_)
