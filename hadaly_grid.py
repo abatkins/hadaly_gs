6f5928eeebc994bfedad9d996b8a8175597da75e
Alex Atkins <alex04634@gmail.com>
2016-04-17 16:02:11 -0400

    param change

--- hadaly_grid.py
+++ hadaly_grid.py
@@ -57,10 +57,10 @@ def main(prod, nested):
     }
     f1_scorer = make_scorer(f1_score, average='samples')
 
-    custom_cv = ShuffleSplit(len(y_train), n_iter=5, test_size=0.01, random_state=0) # iters should be higher
+    custom_cv = ShuffleSplit(len(y_train), n_iter=5, test_size=0.20, random_state=0) # iters should be higher
     #custom_cv = 5
     #custom_inner_cv = 5
-    custom_inner_cv=lambda _x, _y: ShuffleSplit(len(y_train)*.99, n_iter=3, test_size=0.01, random_state=1)
+    custom_inner_cv=lambda _x, _y: ShuffleSplit(int(len(y_train)*.99), n_iter=3, test_size=0.01, random_state=1)
 
     if nested:
         model_tunning = NestedGridSearchCV(model_to_set,
